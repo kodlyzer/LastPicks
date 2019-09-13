3862dd15658fa20eb972922788f97d77ce5103ff
anuja.anu <anuja.anu@kdi.kongsberg.com>
2019-09-13 14:29:13 +0530

    ng-content added.

--- Lpicks-Core/src/app/core/core.module.ts
+++ Lpicks-Core/src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
-import { TabsModule } from './tabs/tabs.module';
+// import { TabsModule } from './tabs/tabs.module';
 import { DropdownModule } from './dropdown/dropdown.module';
 import { ClickOutsideModule } from './click-outside/click-outside.module';
-const MODULE=[TabsModule,DropdownModule, ClickOutsideModule];
+import { TabModule } from './tab/tab.module';
+const MODULE=[TabModule,DropdownModule, ClickOutsideModule];
 @NgModule({
     imports:[...MODULE],
-    exports:[...MODULE]
+    exports:[...MODULE],
+    declarations: []
 })
 export class CoreModule{
     
--- Lpicks-Core/src/app/demo/tabs-demo/tabs-demo.component.ts
+++ Lpicks-Core/src/app/demo/tabs-demo/tabs-demo.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'kdi-tabs-demo',
   templateUrl: './tabs-demo.component.html',
-  styleUrls: ['./tabs-demo.component.sass']
+  styleUrls: ['./tabs-demo.component.scss']
 })
 export class TabsDemoComponent implements OnInit {
   // demotabs="KOGNIFAI DESIGN SYSTEM";
