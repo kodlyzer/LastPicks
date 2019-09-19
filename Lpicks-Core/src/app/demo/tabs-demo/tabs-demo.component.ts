cb80785748444709570f7799725ebe572074a5ef
anuja.anu <anuja.anu@kdi.kongsberg.com>
2019-09-19 14:52:51 +0530

    Styling Changed

--- Lpicks-Core/src/app/demo/tabs-demo/tabs-demo.component.ts
+++ Lpicks-Core/src/app/demo/tabs-demo/tabs-demo.component.ts
@@ -6,42 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./tabs-demo.component.scss']
 })
 export class TabsDemoComponent implements OnInit {
-  // demotabs="KOGNIFAI DESIGN SYSTEM";
- arr = {
-    items: [
-    {
-      "theme": "foam",
-      "classes": null,
-      "txt": "Home",
-      "href": "#",
-      "active": false,
-      "icon": {
-        "size": "base",
-        "symbol": "box"
-      }
-    },
-    {
-      "txt": "Applications",
-      "href": "#",
-    },
-    {
-      "txt": "GSN Management",
-      "href": "#"
-    },
-    {
-      "txt": "Node Offshore",
-      "href": "#"
-    },
-    {
-      "txt": "Shipping",
-      "href": "#"
-    },
-    {
-      "txt": "Tracking",
-      "href": "#"
-    }
-  ]
-}
+ 
   constructor() { }
 
   ngOnInit() {
